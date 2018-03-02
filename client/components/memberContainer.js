8b40b56c89f4e9a51da4bf3ae61b77d57dd407b3
ashifa454 <ashifa454@gmail.com>
2018-03-03 00:12:58 +0530

    Chat Added

--- client/components/memberContainer.js
+++ client/components/memberContainer.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {List,Image,Message,Form,Input,Icon,Segment} from 'semantic-ui-react';
+import {List,Image,Message,Form,Input,Icon,Segment,Feed} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {ActionCreater} from '../app/actions';
@@ -8,7 +8,8 @@ class MemberContainer extends Component{
         super(props);
         this.state={
             usernam:null,
-            isRegistered:false
+            isRegistered:false,
+            message:[1,2,3,4,5,6,7,8]
         }
         this._handleFormSubmit=this._handleFormSubmit.bind(this);
         this._handleInput=this._handleInput.bind(this);
@@ -41,13 +42,33 @@ class MemberContainer extends Component{
             {(this.props.members&&this.props.members.length>0)?<Form onSubmit={this._handleFormSubmit}>
                 <Form.Input label='What`s Your Good Name' name="username" onChange={this._handleInput}/>
                 <Form.Button type={'success'}>Get Started</Form.Button>
-              </Form>:<div><Segment basic>
-
+              </Form>:<div>
+                  <Segment attached basic style={{backgroundColor:'#FFFFFF',minHeight:'500px',maxHeight:'500px',overflowY:'scroll'}}>
+                  
+                  <Feed size='small'>
+                      {
+                          this.state.message.map((item)=>{
+                              return <Feed.Event>
+      <Feed.Label icon='smile' />
+      <Feed.Content>
+        <Feed.Summary>
+        <Feed.User>Elliot Fu</Feed.User>
+          <Feed.Date>3 days ago</Feed.Date>
+        </Feed.Summary>
+        <Feed.Extra text>
+          I'm having a BBQ this weekend. Come by around 4pm if you can.
+        </Feed.Extra>
+      </Feed.Content>
+    </Feed.Event>
+    
+                          })
+                      }
+                </Feed>
               </Segment>
               <Input
               fluid
-    icon={<Icon name='search' inverted circular link />}
-    placeholder='Search...'
+    icon={<Icon name='send' inverted circular link />}
+    placeholder='Type Your Message'
   /></div>
                 }
           </div>
